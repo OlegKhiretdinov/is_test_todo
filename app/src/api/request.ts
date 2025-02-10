af2c00482cc2dbeb6ad2e90bfd136b12fce58fa1
OlegKhiretdinov <oleg@oleg.win>
2025-02-10 17:54:53 +0300

    feat: добавлены запросы

--- app/src/api/request.ts
+++ app/src/api/request.ts
@@ -1,8 +1,25 @@
 const baseUrl = "http://localhost:3001/seminars";
 
 export const seminarsListRequest = async () => {
-  const data = await fetch(`${baseUrl}`, {
+  const data = await fetch(baseUrl, {
     method: "GET",
+  });
+
+  return data.json();
+};
+
+export const deleteSeminar = async (id: string) => {
+  const data = await fetch(`${baseUrl}/${id}`, {
+    method: "DELETE",
+  });
+
+  return data.json();
+};
+
+export const editSeminar = async () => {
+  const data = await fetch(baseUrl, {
+    method: "PUT",
+    body: JSON.stringify({}),
   });
 
   return data.json();
